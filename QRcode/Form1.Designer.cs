c08c7c24a355e13c7b769355efeae31328cf23fc
vitos <vitos@SHPET>
2022-11-10 17:29:41 +0500

    Добавьте файлы проекта.

---
+++ QRcode/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System.Drawing;
+
+namespace QRcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bSave = new System.Windows.Forms.Button();
+            this.bDownload = new System.Windows.Forms.Button();
+            this.bRecognize = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.bCreate = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bSave
+            // 
+            resources.ApplyResources(this.bSave, "bSave");
+            this.bSave.Name = "bSave";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BSave_MouseClick);
+            // 
+            // bDownload
+            // 
+            resources.ApplyResources(this.bDownload, "bDownload");
+            this.bDownload.Name = "bDownload";
+            this.bDownload.UseVisualStyleBackColor = true;
+            this.bDownload.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bDownload_MouseClick);
+            // 
+            // bRecognize
+            // 
+            resources.ApplyResources(this.bRecognize, "bRecognize");
+            this.bRecognize.Name = "bRecognize";
+            this.bRecognize.UseVisualStyleBackColor = true;
+            this.bRecognize.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bRecognize_MouseClick);
+            // 
+            // label
+            // 
+            resources.ApplyResources(this.label, "label");
+            this.label.Name = "label";
+            // 
+            // bCreate
+            // 
+            resources.ApplyResources(this.bCreate, "bCreate");
+            this.bCreate.Name = "bCreate";
+            this.bCreate.UseVisualStyleBackColor = true;
+            this.bCreate.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BCreate_MouseClick);
+            // 
+            // input
+            // 
+            resources.ApplyResources(this.input, "input");
+            this.input.Name = "input";
+            // 
+            // picture
+            // 
+            this.picture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.picture, "picture");
+            this.picture.Name = "picture";
+            this.picture.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.bCreate);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.bRecognize);
+            this.Controls.Add(this.bDownload);
+            this.Controls.Add(this.bSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bDownload;
+        private System.Windows.Forms.Button bRecognize;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button bCreate;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
---
+++ QRcode/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec.Data;
+using MessagingToolkit.QRCode.Codec;
+
+
+namespace QRcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BCreate_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            string qrtext = input.Text;
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap qrcode = encoder.Encode(qrtext);
+            picture.Image = qrcode as Image;
+        }
+
+        private void BSave_MouseClick(object sender, MouseEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                picture.Image.Save(save.FileName);
+            }
+        }
+
+        private void bDownload_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            OpenFileDialog load = new OpenFileDialog();
+            if (load.ShowDialog() == DialogResult.OK)
+            {
+                picture.ImageLocation = load.FileName;
+            }
+        }
+
+        private void bRecognize_MouseClick(object sender, MouseEventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            MessageBox.Show(decoder.Decode(new QRCodeBitmapImage(picture.Image as Bitmap)));
+        }
+    }
+}
